f18fa177ae50fb059e68a4fbd25c40b336505e51
Michael Tew <mike.tew@gmail.com>
2020-05-19 08:32:03 -0700

    Dynamically add athlete data

---
+++ script.js
@@ -0,0 +1,12 @@
+// Filter and sort athlete data
+const newData = data
+  .filter((athlete) => athlete.mileage > 0)
+  .sort((a, b) => b.mileage - a.mileage);
+
+// Create table rows with new data
+const table = document.getElementById('tbody');
+newData.map((athlete, index) => {
+  const row = table.insertRow(index)
+  row.insertCell(0).innerHTML = athlete.name;
+  row.insertCell(1).innerHTML = athlete.mileage;
+});
